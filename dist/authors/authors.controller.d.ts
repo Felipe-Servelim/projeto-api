af5b358e3f175a2c239f5975aab116377af0c8e7
Felipe Servelim <143188639+Felipe-Servelim@users.noreply.github.com>
2024-10-15 11:52:44 -0300

    Delete dist directory

--- dist/authors/authors.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthorsService } from './authors.service';
-import { Author } from './author.entity';
-export declare class AuthorsController {
-    private readonly authorsService;
-    constructor(authorsService: AuthorsService);
-    findAll(): Author[];
-    findOne(id: string): Author;
-}
--- dist/authors/authors.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Author } from './author.entity';
-export declare class AuthorsService {
-    private authors;
-    findAll(): Author[];
-    findOne(id: number): Author;
-}
